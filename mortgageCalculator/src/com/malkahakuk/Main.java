8563e80cd3ed172a1348b30cc8bca204d2e1c322
Malka Hakuk <31603690+malkahakuk@users.noreply.github.com>
2022-02-10 17:15:01 -0500

    Java Mortgage Calculator

---
+++ mortgageCalculator/src/com/malkahakuk/Main.java
@@ -0,0 +1,75 @@
+package com.malkahakuk;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Main {
+    final static byte MONTHS_IN_YEAR = 12;
+    final static byte PERCENT = 100;
+
+    public static void main(String[] args) {
+
+        int principal = (int)readNumber("Principal: ", 1_000, 1_000_000);
+        float annualInterest = (float)readNumber("Annual Interest Rate: ", 1, 30);
+        byte years = (byte)readNumber("Period (Years): ", 1, 30);
+
+        printMortgage(principal, annualInterest, years);
+        printPaymentSchedule(principal, annualInterest, years);
+    }
+
+    public static void printMortgage(int principal, float annualInterest, byte years) {
+        double mortgage = calculateMortgage(principal, annualInterest, years);
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);;
+        System.out.println("\n MORTGAGE \n -------- \n Monthly payments: \n " + mortgageFormatted);
+    }
+
+    public static void printPaymentSchedule(int principal, float annualInterest, byte years) {
+        System.out.println("\n PAYMENT SCHEDULE \n ----------------");
+        for (short month = 1; month <= years * MONTHS_IN_YEAR; month++){
+            double balance = calculateBalance(principal, annualInterest, years, month);
+            System.out.println(NumberFormat.getCurrencyInstance().format(balance));
+        }
+    }
+
+    public static double readNumber(String prompt, int min, int max) {
+        Scanner scanner = new Scanner(System.in);
+        double value;
+        while (true) {
+            System.out.print(prompt);
+            value = scanner.nextInt();
+            if (value >= min && value <= max)
+                break;
+            System.out.println("Enter a value between " + min + " and " + max);
+        }
+        return value;
+    }
+
+    public static double calculateBalance(
+            int principal,
+            float annualInterest,
+            byte years,
+            short numberOfPaymentsMade
+    ){
+        short numberOfPayments = (short)(years * MONTHS_IN_YEAR);
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+
+        double balance = principal
+                * (Math.pow(1 + monthlyInterest, numberOfPayments) - Math.pow(1 + monthlyInterest, numberOfPaymentsMade))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) -1);
+
+        return balance;
+    }
+
+    public static double calculateMortgage(
+            int principal,
+            float annualInterest,
+            byte years){
+        short numberOfPayments = (short)(years * MONTHS_IN_YEAR);
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+        double mortgage = principal
+                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) -1);
+        return mortgage;
+    }
+}
